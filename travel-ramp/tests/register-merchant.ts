39d97628ed04e6cec07d1af683e9ef3f286d58f7
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2026-06-16 11:55:52 +0100

    fix: update tests

--- travel-ramp/tests/register-merchant.ts
+++ travel-ramp/tests/register-merchant.ts
@@ -1,27 +1,26 @@
 import * as anchor from "@anchor-lang/core";
 import { expect } from "chai";
-import { confirmTx, findMerchantPda, program, provider } from "./helpers";
+import {
+  confirmTx,
+  findMerchantPda,
+  fundAccount,
+  program,
+  provider,
+} from "./helpers";
 
 describe("register_merchant", () => {
-  it("initializes a merchant account", async () => {
-    const payer = anchor.web3.Keypair.generate();
+  it("initializes a merchant account for a wallet", async () => {
+    const payer = provider.wallet.publicKey;
     const merchantWallet = anchor.web3.Keypair.generate().publicKey;
     const [merchantAccount, merchantBump] = findMerchantPda(merchantWallet);
-
-    const airdropTx = await provider.connection.requestAirdrop(
-      payer.publicKey,
-      1 * anchor.web3.LAMPORTS_PER_SOL,
-    );
-    await confirmTx(airdropTx);
 
     const tx = await program.methods
       .registerMerchant(merchantWallet)
       .accountsPartial({
-        payer: payer.publicKey,
+        payer,
         merchantAccount,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([payer])
       .rpc();
     await confirmTx(tx);
 
@@ -29,7 +28,7 @@ describe("register_merchant", () => {
       merchantAccount,
     );
 
-    expect(account.operator.equals(payer.publicKey)).to.equal(true);
+    expect(account.operator.equals(payer)).to.equal(true);
     expect(account.wallet.equals(merchantWallet)).to.equal(true);
     expect(account.status).to.deep.equal({ approved: {} });
     expect(account.totalReceived.toNumber()).to.equal(0);
